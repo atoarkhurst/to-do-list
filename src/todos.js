64db26e6eefcd33b175c707645b7f6fc7363d2cd
Ato Arkhurst <atoparkhurst@gmail.com>
2023-12-10 13:47:48 -0500

    update interface

--- src/todos.js
+++ src/todos.js
@@ -11,3 +11,25 @@ let createTodo( title, description, dueDate, priority ) {
     return todo;
     
 }
+
+const addTaskBtn = document.querySelector('.add-task-btn');
+
+function getTodoInfo() { 
+
+    //when user clicks button make todo name field appear
+
+    const taskTitle = 
+
+    //make todo description field appear
+
+
+    //make todo date field appear 
+
+
+    //make todo priority field appear
+
+
+    // store form info in new Todo object 
+
+
+}
