d2ce03bc4e3d814b90f1e27e8d81429d239c97c4
Ato Arkhurst <atoparkhurst@gmail.com>
2024-01-30 06:57:36 -0500

    create edit task function

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,5 @@
+import { updateTaskDisplay } from "./display";
+
 export function createProject(title){
 
     return {
@@ -14,4 +16,34 @@ export function createProject(title){
 
 export function removeTask(project, taskID) {
     project.tasks = project.taks.filter(task => task.id !== taskID);
+}
+
+
+export function editTask ( project, taskID, title, description, dueDate, priority ) {
+
+    const task = project.tasks.find(task => task.id ==  taskID);
+
+    if (!task) {
+        console.error('Task not found');
+        return;
+    }
+
+    if (title) {
+        task.title = title;
+    } 
+
+    if (description) {
+        task.description = description;
+    }
+
+    if (dueDate) {
+        task.dueDate = dueDate;
+    }
+
+    if (priority) {
+        task.title = priority;
+    }
+
+    updateTaskDisplay(task, taskID); 
+
 }
